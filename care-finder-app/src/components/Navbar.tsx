67ed31e7ed552bd4dee8d78f1905c4adbc7ee529
john ogheneochuko <ochustech56@gmail.com>
2024-07-25 21:13:12 +0100

    changes the navbar layout

--- care-finder-app/src/components/Navbar.tsx
+++ care-finder-app/src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import RegisterLogin from "./RegisterLogin"
 
 const Navbar = () => {
 
     return (
      <div>
-<nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+<nav className="navbar  navbar-expand-lg bg-body-tertiary" data-bs-theme="">
   <div className="container-fluid">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -18,11 +19,11 @@ const Navbar = () => {
         <li className="nav-item">
         <Link  href="/about" className="nav-link" aria-current="page">About</Link>
         </li>
+        <li className="nav-item">
+        <Link  href="/profile" className="nav-link" aria-current="page">Profile</Link>
+        </li>
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+     <RegisterLogin />
     </div>
   </div>
 </nav>
